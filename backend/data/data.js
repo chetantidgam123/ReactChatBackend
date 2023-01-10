09710bb6574cc2b6ba1ffa3d216b8a4161850f3d
chetan <chetan@shipyaari.com>
2023-01-10 19:10:15 +0530

    push

---
+++ backend/data/data.js
@@ -0,0 +1,22 @@
+const chats = [
+    {
+        isGroupChat: false,
+        user: [
+            {
+                name: "Chetan",
+                email:"dada@da.com"
+            },
+            {
+                name: "Chetan",
+                email:"dada@da.com"
+            },
+            {
+                name: "Chetan",
+                email:"dada@da.com"
+            }
+        ],
+        _id: "617a077e18c25468bc7c4dd4",
+        chatname:"Jhone Doe"
+    }
+]
+module.exports = chats
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const chats = require("./data/data")
+dotenv.config()
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Api is running")
+})
+app.get('/api/chats', (req, res) => {
+    res.send(chats)
+})
+app.get('/api/chats/:id', (req, res) => {
+    const single_data = chats.find(c => c._id === req.params.id)
+    res.send(single_data)
+})
+const PORT  =process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server listen on port ${PORT}`);
+})
